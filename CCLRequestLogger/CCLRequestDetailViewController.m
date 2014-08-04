cb9cbd5a74c7af2257892d2d5b5ef2d9149f50cd
Kyle Fuller <inbox@kylefuller.co.uk>
2014-08-04 16:58:04 +0100

    [Viewer] Show headers

--- CCLRequestLogger/CCLRequestDetailViewController.m
+++ CCLRequestLogger/CCLRequestDetailViewController.m
@@ -10,7 +10,16 @@
 #import "CCLRequestDetailViewController.h"
 
 
+typedef NS_ENUM(NSInteger, CCLRequestDetailSegments) {
+    CCLRequestDetailRequestSegments,
+    CCLRequestDetailResponseSegments
+};
+
+
 @interface CCLRequestDetailViewController ()
+
+@property (nonatomic, strong) NSDictionary *request;
+@property (nonatomic, strong) NSDictionary *headers;
 
 @end
 
@@ -26,11 +35,60 @@
     UISegmentedControl *segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"Request", @"Response"]];
     self.navigationItem.titleView = segmentedControl;
     [segmentedControl addTarget:self action:@selector(changedSegment:) forControlEvents:UIControlEventValueChanged];
-    segmentedControl.selectedSegmentIndex = 0;
+    segmentedControl.selectedSegmentIndex = CCLRequestDetailRequestSegments;
 }
 
 - (void)changedSegment:(UISegmentedControl *)segmentedControl {
+    [self.tableView reloadData];
+}
 
+#pragma mark -
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    UISegmentedControl *segmentedControl = (UISegmentedControl *)self.navigationItem.titleView;
+
+    NSDictionary *headers;
+    switch ((CCLRequestDetailSegments)segmentedControl.selectedSegmentIndex) {
+        case CCLRequestDetailRequestSegments:
+            headers = self.recording.request.allHTTPHeaderFields;
+            break;
+        case CCLRequestDetailResponseSegments: {
+            NSHTTPURLResponse *response = self.recording.response;
+            headers = response.allHeaderFields;
+            break;
+        }
+    }
+
+    return [headers count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Cell"];
+
+    UISegmentedControl *segmentedControl = (UISegmentedControl *)self.navigationItem.titleView;
+    NSDictionary *headers;
+    switch ((CCLRequestDetailSegments)segmentedControl.selectedSegmentIndex) {
+        case CCLRequestDetailRequestSegments:
+            headers = self.recording.request.allHTTPHeaderFields;
+            break;
+        case CCLRequestDetailResponseSegments: {
+            NSHTTPURLResponse *response = self.recording.response;
+            headers = response.allHeaderFields;
+            break;
+        }
+    }
+
+    NSString *key = headers.allKeys[indexPath.row];
+    NSString *value = headers[key];
+
+    cell.textLabel.text = key;
+    cell.detailTextLabel.text = value;
+
+    return cell;
 }
 
 @end
